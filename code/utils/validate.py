21c600d396fd30751ce352925498d265199ec23c
Beiwen Tian <tianbeiwen88@gmail.com>
2023-07-27 18:16:55 +0800

    add helper text for args/config entries

--- code/utils/validate.py
+++ code/utils/validate.py
@@ -11,10 +11,11 @@ logger = logging.getLogger('validate')
 from tqdm import tqdm
 
 
-def validate(model, val_loader: DataLoader, criterion, metrics=[], writer=None, global_iter=None):
+def validate(model, val_loader: DataLoader, criterion=None, metrics=[], writer=None, global_iter=None):
     if get_local_rank() == 0:
         model.eval()
         with torch.no_grad():
+            loss_sum = 0
             metric_objs = [
                 metric(
                     val_loader.dataset.num_train_classes,
@@ -22,19 +23,31 @@ def validate(model, val_loader: DataLoader, criterion, metrics=[], writer=None, 
                     val_loader.dataset.train_class_names,
                 ) for metric in metrics
             ]
-            loss_sum = 0
+
             for idx, (inputs, labels, extras) in enumerate(tqdm(val_loader)):
                 output = model(to_device(inputs, device))
-                loss_sum += criterion(output, to_device(labels, device)).item()
                 pred = output.argmax(dim=1).cpu()
                 if 'maps' in extras:
                     pred = pred[extras['maps'][1]]
                     labels = labels[extras['maps'][1]]
-                metric.record(pred, labels)
 
-        metric_results = {metric.NAME: metric.calc() for metric in metric_objs}
+                if criterion is not None:
+                    loss_sum += criterion(output, to_device(labels, device)).item()
+
+                for metric in metric_objs:
+                    metric.record(pred, labels)
+
+        loss_sum /= len(val_loader)
+
+        metric_results = {}
         for metric in metric_objs:
+            metric_results[metric.NAME] = metric.calc()
             metric.log(logger, writer=writer, global_iter=global_iter, name_prefix='val/')
-        return loss_sum / len(val_loader), metric_results
+
+        return loss_sum, metric_results
     else:
         return 0, {}
+
+
+if __name__ == '__main__':
+    pass
