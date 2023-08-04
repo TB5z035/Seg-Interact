496f63e1da9b74dd0e2e5f7f1be9bbde74494e64
caiz <caizicharlesofficial@gmail.com>
2023-08-04 16:21:55 +0800

    param change

--- code/mytest.py
+++ code/mytest.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import os.path as osp
 import re
 import torch
 '''a = np.array([1,2,3])
@@ -9,12 +10,7 @@ a = np.array([2,2,3])
 np.save('/home/Guest/caiz/Seg-Interact/configs/testing/my.npy', a)
 a = np.load('/home/Guest/caiz/Seg-Interact/configs/testing/my.npy')
 print(a)'''
-
-
-def f7(seq):
-    seen = set()
-    seen_add = seen.add
-    return [x for x in seq if not (x in seen or seen_add(x))]
-
-
-print(f7([1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 6, 6, 6, 6, 7, 7, 7]))
+a = np.array([1, 2, 3, 4, 5, 6])
+b = np.array([0, 0, 1])
+c = np.delete(np.arange(len(a)), b)
+print(c)
