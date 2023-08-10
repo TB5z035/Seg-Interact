341abed6df36d6373f8b0319424270e6b34c13b5
caiz <caizicharlesofficial@gmail.com>
2023-08-10 13:51:01 +0800

    content changes for testing and added folders

--- code/mytest.py
+++ code/mytest.py
@@ -3,14 +3,19 @@ import os
 import os.path as osp
 import re
 import torch
-'''a = np.array([1,2,3])
-os.makedirs('/home/Guest/caiz/Seg-Interact/configs/testing', exist_ok=True)
-np.save('/home/Guest/caiz/Seg-Interact/configs/testing/my.npy', a)
-a = np.array([2,2,3])
-np.save('/home/Guest/caiz/Seg-Interact/configs/testing/my.npy', a)
-a = np.load('/home/Guest/caiz/Seg-Interact/configs/testing/my.npy')
-print(a)'''
-a = np.array([1, 2, 3, 4, 5, 6])
-b = np.array([0, 0, 1])
-c = np.delete(np.arange(len(a)), b)
-print(c)
+'''
+x = np.array([[1, 2, 3], [4, 5, 6]])
+y = np.divide(x[:, 2], 2)
+y = np.expand_dims(y, axis=1)
+print(x[:, :2])
+print(y)
+x = np.concatenate((x[:, :2], y), axis=1)
+print(x)
+'''
+x = np.array([[1, 2, 3], [4, 5, 6]])
+y = x[:, 1:]
+print(y)
+y[:, 0] = 0
+print(y)
+
+print(9 % 8)
