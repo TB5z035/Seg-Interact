1a67eec55368c238487ec7826ba542781a0b4b15
caiz <caizicharlesofficial@gmail.com>
2023-09-21 00:20:33 +0800

    added sp eval in demo

--- code/mytest.py
+++ code/mytest.py
@@ -3,11 +3,31 @@ import os
 import os.path as osp
 import re
 import torch
+import torch.nn as nn
 from itertools import repeat
 import argparse
 import os.path as osp
 import yaml
-
-a = np.array([1, 2, 3])
-a.pop(3)
-print(a)
+'''
+lin = nn.Linear(3,9)
+m = torch.rand((2,3,3))
+print(m)
+qkv = lin(m)
+print(qkv.shape)
+#             B N 3 H C          3 B H N C
+qkv = qkv.reshape(2,3,3,1,3).permute(2,0,3,1,4)
+q = qkv[0]
+k = qkv[1]
+print(k.shape)
+k = k.transpose(-2,-1)
+print(k.shape)
+s = q @ k
+print(s.shape)
+'''
+a = torch.rand((2, 1, 2, 3))
+b = torch.rand((2, 1, 2, 3))
+print(b.shape)
+b = b.transpose(-2, -1)
+print(b.shape)
+c = a @ b
+print(c.shape)
--- code/network/Superpoint_MAE.py
+++ code/network/Superpoint_MAE.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import torch.nn as nn
 
 from . import register_network
 
@@ -33,3 +34,45 @@ class Superpoint_MAE():
 
     def __init__(self):
         raise NotImplementedError
+
+
+'''Stage 2'''
+
+
+class MPN_MLP(nn.module):
+
+    def __init__(self, mlp_in_dim=8):
+        super().__init__()
+        self.mlp_in_dim = mlp_in_dim
+        self.fc_layer = nn.Linear(self.in_dim, 1)
+        self.softmax = nn.Softmax(dim=0)
+
+    def forward(self, encoder_features):
+        logits = self.fc_layer(encoder_features)
+        print(logits.shape)
+        scores = self.softmax(torch.squeeze(logits, dim=1))
+        print(scores, scores.shape)
+        return scores
+
+
+class MPN_Encoder(nn.module):
+
+    def __init__(self, encoder_in_dim=10, encoder_out_dim=8):
+        super().__init__()
+        self.encoder_in_dim = encoder_in_dim
+        self.encoder_out_dim = encoder_out_dim
+        self.qkv = nn.Linear(self.encoder_in_dim, self.encoder_out_dim)
+
+    def forward(self, point_features):
+        return encoder_features
+
+
+@register_network('MPN')
+class MPN(MPN_Encoder, MPN_MLP):
+
+    def __init__(self, dims):
+        self.encoder = MPN_Encoder()
+        self.mlp = MPN_MLP()
+
+    def forward(self):
+        return mask_sp_indices
