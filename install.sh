e56ecaa6cb7f2509c03e8ba217214e501f471ec7
Zi Cai <98086762+caizicharles@users.noreply.github.com>
2024-02-29 04:38:09 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 conda create -n $ENV_NAME python=3.9
 conda activate $ENV_NAME
 
-# conda install pytorch=1.9.0 torchvision cudatoolkit=11.1 -c pytorch -c nvidia
 pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
 pip install yapf plyfile pyyaml tqdm tensorboardX torch-tb-profiler scipy
 
@@ -9,3 +8,29 @@ export CUDA_HOME=/usr/local/cuda-11.1
 git clone https://github.com/NVIDIA/MinkowskiEngine.git
 cd MinkowskiEngine
 python setup.py install --blas_include_dirs=${CONDA_PREFIX}/include --blas=openblas
+
+# Superpoint Transformer Related
+conda install pip nb_conda_kernels -y
+pip install matplotlib
+pip install plotly==5.9.0
+pip install "jupyterlab>=3" "ipywidgets>=7.6" jupyter-dash
+pip install "notebook>=5.3" "ipywidgets>=7.5"
+pip install ipykernel
+pip install torchmetrics==0.11.4
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-${TORCH}+cu${CUDA_MAJOR}${CUDA_MINOR}.html
+pip install plyfile
+pip install h5py
+pip install colorhash
+pip install seaborn
+pip3 install numba
+pip install pytorch-lightning
+pip install pyrootutils
+pip install hydra-core --upgrade
+pip install hydra-colorlog
+pip install hydra-submitit-launcher
+pip install rich
+pip install torch_tb_profiler
+pip install wandb
+pip install open3d
+pip install gdown
+pip install ipyfilechooser
